d0516d657d497890b9cffbae7898cd23473dfb67
张鲁燕 <zhangluyan@jsh.com>
2021-06-23 21:34:28 +0800

    项目初始1

---
+++ src/pages/list/list.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class list extends Component {
+    render() {
+        return (
+            <div>
+                List
+            </div>
+        )
+    }
+}
+
+export default list

3877e34f5b591afccb6ac58486cbfef44d5316d9
abc <tianxiamall@163.com>
2021-06-24 17:11:53 +0800

    edit header

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -6,7 +6,7 @@ export class nav extends Component {
     render() {
         return (
         <Row style={{ justifyContent: "center", alignItems: "center",width:"100%",backgroundColor:"white"}} >
-          <Col xxl={15} xl={19} className="header_nav">
+          <Col xxl={15} xl={19} className="nav">
               <Menu theme="light" mode="horizontal" >
                   <Menu.Item>信息技术</Menu.Item>
                   <Menu.Item>经济学</Menu.Item>

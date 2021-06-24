ee784890010c490757ceb805944486aa430cd7e7
张鲁燕 <zhangluyan@jsh.com>
2021-06-24 22:10:00 +0800

    edit footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,15 +1,21 @@
-import React, { Component } from 'react'
-import { Layout } from 'antd'
+import React, { Component } from "react";
+import { Layout, Row, Col } from "antd";
+import "./footer.less";
 const { Footer } = Layout;
 
 export class MyFooter extends Component {
-    render() {
-        return (
-          <Footer>
-              Footer
-          </Footer>
-        )
-    }
+  render() {
+    return (
+      <Footer style={{backgroundColor:"wheat",marginTop:30}}>
+        <Row style={{ justifyContent: "center", alignItems: "center"}}>
+          <Col className="footer">
+            <span>本站所有资源均来自互联网，如有侵权，请联系站长删除！</span>
+            <i>网站备案/许可证号：<a href="https://beian.miit.gov.cn/">鲁ICP备18001334号-4</a></i>
+          </Col>
+        </Row>
+      </Footer>
+    );
+  }
 }
 
-export default MyFooter
+export default MyFooter;

219664047b553a029c95e9e2f13e14162acf0dd9
abc <tianxiamall@163.com>
2021-06-24 16:36:12 +0800

    edit header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Layout, Row, Col } from "antd";
+import { Layout, Row, Col, Menu  } from "antd";
 
 import "./header.less";
 const { Header } = Layout;
@@ -7,10 +7,19 @@ export class MyHeader extends Component {
   render() {
     return (
       <Header>
-        <Row style={{justifyContent:"center",alignItems:"center"}}>
+        <Row style={{ justifyContent: "center", alignItems: "center" }}>
           <Col className="header">
-          <span>资料库</span>
-          <i>信息技术电子资料分享下载</i>
+            <span>资料库</span>
+            <i>信息技术电子资料分享下载</i>
+          </Col>
+        </Row>
+        <Row style={{ justifyContent: "center", alignItems: "center" }}>
+          <Col xxl={15} xl={19} className="header_nav">
+              <Menu theme="light" mode="horizontal" >
+                  <Menu.Item>信息技术</Menu.Item>
+                  <Menu.Item>经济学</Menu.Item>
+                  <Menu.Item>有声小说</Menu.Item>
+              </Menu>
           </Col>
         </Row>
       </Header>

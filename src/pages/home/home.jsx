255ca4010d3966f01c35b340525e41c520ea2928
张鲁燕 <zhangluyan@jsh.com>
2021-06-24 21:09:51 +0800

    调整样式

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { Row, Col } from "antd";
 import Book from "@/components/book/book";
+import "./home.less";
 
 export class Home extends Component {
   render() {
     return (
       <Row justify="center">
-        <Col xxl={15} xl={19}>
+        <Col xxl={15} xl={19} className="contentContainer">
           <Row>
             <Book
               src="https://ziliaok.cn/uploads/jenkins2quanweizhinan/main.jpg"

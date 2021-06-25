e5a7a4846a964ff47686871533506aace92ed747
张鲁燕 <zhangluyan@jsh.com>
2021-06-25 18:57:42 +0800

    info 页面布局

--- src/pages/info/info.jsx
+++ src/pages/info/info.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
+import { Row, Col } from "antd";
+import MySider from "@/components/sider/sider";
 
 export class Info extends Component {
     render() {
         return (
-            <div>
-                Info
-            </div>
+      <Row justify="center">
+        <Col xxl={15} xl={19} className="contentContainer">
+          <Row>
+            <Col xl={18}>
+              <Row>
+INFO_Content
+              </Row>
+            </Col>
+            <Col xl={6}>
+              <MySider />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
         )
     }
 }

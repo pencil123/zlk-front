9afc83d6f7c6ac64d6a9944c10c6f89ccbb11301
abc <tianxiamall@163.com>
2021-06-25 16:55:08 +0800

    add Info page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import { Layout } from 'antd'
+import { Layout } from "antd";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 
 import Home from "@/pages/home/home";
+import Info from "@/pages/info/info";
 import MyHeader from "@/components/header/header";
-import MyFooter from './components/footer/footer';
-import './App.css';
+import MyFooter from "./components/footer/footer";
+import "./App.css";
 
 const { Content } = Layout;
 
 function App() {
   return (
+    <BrowserRouter>
       <Layout>
-          <MyHeader />
-          <Content>
-              <Home />
-          </Content>
-          <MyFooter />
+        <MyHeader />
+        <Content>
+          <Switch>
+            <Route path="/" exact component={Home}/>
+            <Route path="/info" exact component={Info}/>
+          </Switch>
+        </Content>
+        <MyFooter />
       </Layout>
+    </BrowserRouter>
   );
 }
 

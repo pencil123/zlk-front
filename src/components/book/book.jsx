7c7f7bcd46c19ca22b74f4f2a39432e90696d179
张鲁燕 <zhangluyan@jsh.com>
2021-06-26 15:08:40 +0800

    edit pages

--- src/components/book/book.jsx
+++ src/components/book/book.jsx
@@ -6,7 +6,7 @@ export class Book extends Component {
   render() {
     return (
       <>
-        <Col xxl={6} xl={6} sm={12} md={12} xs={12} className="book">
+        <Col xxl={6} xl={6}  md={8} sm={12} xs={12} className="book">
           <img src="https://ziliaok.cn/uploads/jenkins2quanweizhinan/main.jpg" alt="123"></img>
           <span>Jenkins2 权威指南</span>
           <i>【美】布伦特·莱斯特（Brent Laster）,郝树伟等</i>

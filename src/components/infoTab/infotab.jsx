bfe6d27b9cd8fe7deabf32de8ed730192f03059b
abc <tianxiamall@163.com>
2021-06-26 17:41:23 +0800

    edit info tab

--- src/components/infoTab/infotab.jsx
+++ src/components/infoTab/infotab.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Row,Col, Descriptions, Button, Image } from "antd";
+import { Row, Col, Descriptions, Button, Image } from "antd";
 import { DownloadOutlined } from "@ant-design/icons";
 import MyBreadcrumb from "../breadcrumb/breadcrumb";
 import "./infotab.less";
@@ -12,56 +12,56 @@ export class infotab extends Component {
           <MyBreadcrumb />
         </Col>
         <Col span={24}>
-            <Row className="infotab">
-                <Col xxl={8} xl={8}>
-          <div className="info_left">
-            <img
-              src="https://ziliaok.cn/uploads/dockerrongqiyurongqiyun/main.jpg"
-              alt="123"
-            />
-          </div>
-          </Col>
-          <Col xxl={16} xl={16}>
-          <div className="info_right">
-            <Descriptions
-              title="Docker 容器与容器云"
-              bordered
-              column={1}
-              size="small"
-            >
-              <Descriptions.Item label="标题">
-                Docker 容器与容器云
-              </Descriptions.Item>
-              <Descriptions.Item label="作者">
-                浙江大学SEL实验室
-              </Descriptions.Item>
-              <Descriptions.Item label="出版社">
-                人民邮电出版社
-              </Descriptions.Item>
-              <Descriptions.Item label="关键词">
-                Docker,Docker容器,容器云,kubernetes,Docker 容器与容
-              </Descriptions.Item>
-            </Descriptions>
-            <div className="download">
-              <Button
-                type="primary"
-                shape="round"
-                icon={<DownloadOutlined />}
-                size="large"
-              >
-                下载地址
-              </Button>
-            </div>
-            <div className="customer_wechat">
-              <span>
-                所有资源需使用<b>下载码</b>
-                下载；如没有【下载码】，请添加站长微信有偿索取。
-              </span>
-              <Image src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png" />
-              <div style={{ clear: "both" }}></div>
-            </div>
-          </div>
-          </Col>
+          <Row className="infotab">
+            <Col xxl={8} xl={8} lg={8} md={8} sm={8}>
+              <div className="info_left">
+                <img
+                  src="https://ziliaok.cn/uploads/dockerrongqiyurongqiyun/main.jpg"
+                  alt="123"
+                />
+              </div>
+            </Col>
+            <Col xxl={16} xl={16} lg={16} md={16} sm={16}>
+              <div className="info_right">
+                <Descriptions
+                  title="Docker 容器与容器云"
+                  bordered
+                  column={1}
+                  size="small"
+                >
+                  <Descriptions.Item label="标题">
+                    Docker 容器与容器云
+                  </Descriptions.Item>
+                  <Descriptions.Item label="作者">
+                    浙江大学SEL实验室
+                  </Descriptions.Item>
+                  <Descriptions.Item label="出版社">
+                    人民邮电出版社
+                  </Descriptions.Item>
+                  <Descriptions.Item label="关键词">
+                    Docker,Docker容器,容器云,kubernetes,Docker 容器与容
+                  </Descriptions.Item>
+                </Descriptions>
+                <div className="download">
+                  <Button
+                    type="primary"
+                    shape="round"
+                    icon={<DownloadOutlined />}
+                    size="large"
+                  >
+                    下载地址
+                  </Button>
+                </div>
+                <div className="customer_wechat">
+                  <span>
+                    所有资源需使用<b>下载码</b>
+                    下载；如没有【下载码】，请添加站长微信有偿索取。
+                  </span>
+                  <Image src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png" />
+                  <div style={{ clear: "both" }}></div>
+                </div>
+              </div>
+            </Col>
           </Row>
         </Col>
         <Col span={24} style={{ margin: "4px 8px" }}>
@@ -82,8 +82,8 @@ export class infotab extends Component {
           <div
             style={{
               paddingLeft: 8,
-              padding:8,
-              border: "1px solid wheat"
+              padding: 8,
+              border: "1px solid wheat",
             }}
           >
             第1章 Jenkins 2简介 1 Jenkins 2是什么 2 Jenkinsfile 3 声明式流水线 5
